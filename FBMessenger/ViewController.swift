3d95aafee2b4a0bca7e2a3662cebd5f302d3fff8
John Ernest D. Ramos <jeramos.dev@gmail.com>
2017-10-15 17:59:52 +0800

    Initial Commit
    Initial Project Commit

---
+++ FBMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBMessenger
+//
+//  Created by John Ernest Ramos on 15/10/2017.
+//  Copyright © 2017 John Ernest Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
